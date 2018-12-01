8e53000478e00564abbdd14daa56c1aae8489e80
Andrew Sampson <andrew@codeusa523.org>
2018-12-01 16:14:29 -0500

    Pushing day 1 a bit late

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Day1.Solve();
+            Console.Read();
+        }
+    }
+}
